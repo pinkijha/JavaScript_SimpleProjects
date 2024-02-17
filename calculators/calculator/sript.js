9fc6490a7e9215bed871034ea2961c90da6b24eb
“pinki <pinkijha1310@gmail.com>
2024-02-18 00:55:21 +0530

    Calculator

--- calculators/calculator/sript.js
+++ calculators/calculator/sript.js
@@ -4,34 +4,28 @@ let btns = document.querySelectorAll(".btn1");
 let string = "";
 let arr = Array.from(btns);
 arr.forEach((btn) => {
-btn.addEventListener("click", (e) => {
-if(e.target.innerHTML == "="){
-     string = eval(string);
-     inpt.value = string;
-}
-else if(e.target.innerHTML == "C"){
-    string = " ";
-     inpt.value = string;
-
-}
-
-//optional
-else if(e.target.innerHTML == "DEL"){
-    string = string.substring(0, string.length-1);
-    inpt.value = string;
-}
-else if(e.target.innerHTML == "."){
-    if(!string.includes(".")){
-        string += e.target.innerHTML;
-        inpt.value = string;
-
-    }
-}
-else{
-    string += e.target.innerHTML;
-    inpt.value = string;
-}
-})
-})
-
-
+    btn.addEventListener("click", (e) => {
+        if (e.target.innerHTML == "=") {
+            try {
+                string = eval(string);
+                inpt.value = string;
+            } catch (error) {
+                inpt.value = "Error";
+            }
+        } else if (e.target.innerHTML == "C") {
+            string = "";
+            inpt.value = string;
+        } else if (e.target.innerHTML == "DEL") {
+            string = string.substring(0, string.length - 1);
+            inpt.value = string;
+        } else if (e.target.innerHTML == ".") {
+            if (!string.endsWith(".") && !string.includes(".")) {
+                string += e.target.innerHTML;
+                inpt.value = string;
+            }
+        } else {
+            string += e.target.innerHTML;
+            inpt.value = string;
+        }
+    });
+});
